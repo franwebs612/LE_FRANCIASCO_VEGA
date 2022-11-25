21f66c5b36b2eba2b2e3d8d632f8d22a141c2a83
santisan1 <108411999+santisan1@users.noreply.github.com>
2022-11-25 13:07:18 -0300

    a

--- Python 3/prendas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-mainpath = "sample_data"
-filename = "limon.csv"
-fullpath = os.path.join(mainpath,filename)
-  df = pd.read_csv(filename, index_col=0, encoding='latin-1')
-df.head()
